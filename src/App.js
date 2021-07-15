4a3ab4e94ada8d0e31bd43755d6ce0122203a462
Davit Gelashvili <datoogelashvili@gmail.com>
2021-07-15 18:14:05 +0400

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log(event.target.value)
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form className="start" onChange={handleSubmit}>
+      <div className="start__item flex-column">
+        <span className="start__item--title">Select Category:</span>
+        <select className="start__item--select">
+          <option>Category</option>
+          <option value="21">Sports</option>
+          <option value="24">politics</option>
+        </select>
+      </div>
+
+      <div className="start__item flex-column">
+        <span className="start__item--title">Select Difficulty:</span>
+        <select className="start__item--select">
+          <option>Difficulty</option>
+          <option value="easy">Easy</option>
+          <option value="medium">Medium</option>
+          <option value="hard">Hard</option>
+        </select>
+      </div>
+      <input type="submit" value="START" className="btn" />
+    </form>
   );
 }
 
